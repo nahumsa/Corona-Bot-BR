24f0e460d0e7d3cd89a850700f49cdbd7647cdf4
nahumsa <nahumsaa@gmail.com>
2020-03-16 22:00:26 -0300

    Added twitter bot

--- corona_brasil.py
+++ corona_brasil.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from datetime import datetime
 import numpy as np
+import time
 
 class Coronavirus():
     """
@@ -9,35 +10,38 @@ class Coronavirus():
     def __init__(self):
         self.driver = webdriver.Chrome()
     
-    def __str__(self):
+    def update(self):
         try:
             possible_Cases = self.data[0][0]
             confirmed_Cases = self.data[1][0]
             descarted_cases = self.data[2][0]
             deaths = self.data[3][0]
+            now = datetime.now()         
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")    
+
+            #print(f"País: Brasil")
+            #print(f"Casos Possíveis: {possible_Cases}")
+            #print(f"Casos Confirmados: {confirmed_Cases}")            
+            #print(f"Casos Descartados: {descarted_cases}")
+            #print(f"Mortes: {deaths}")
             
-
-            print(f"País: Brasil")
-            print(f"Casos Possíveis: {possible_Cases}")
-            print(f"Casos Confirmados: {confirmed_Cases}")            
-            print(f"Casos Descartados: {descarted_cases}")
-            print(f"Mortes: {deaths}")
-                    
+            return f"País: Brasil\nCasos Possíveis: {possible_Cases}\nCasos Confirmados: {confirmed_Cases}\nCasos Descartados: {descarted_cases}\nMortes: {deaths}\nData e Hora : {dt_string} \nDados de : http://plataforma.saude.gov.br/novocoronavirus/ \n"            
+        
         except:
+            now = datetime.now() 
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            
             print("Something is wrong.")
+            return f"Data e Hora : {dt_string} \nDados de : http://plataforma.saude.gov.br/novocoronavirus/ \n"
 
-        now = datetime.now() 
-        
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")        
-        
-        return f"Data e Hora : {dt_string} \nDados de : http://plataforma.saude.gov.br/novocoronavirus/ \n"
 
     def get_data(self):
         """Getting data from http://plataforma.saude.gov.br/novocoronavirus/.
         """
-        try:        
+        try:                    
             self.data = []
             self.driver.get('http://plataforma.saude.gov.br/novocoronavirus/')
+            time.sleep(5)
             for i in np.arange(1,4+1,1):
                 #order: Possible Cases, Confirmed Cases, descarted cases, deaths
                 dat = self.driver.find_element_by_xpath(f'/html/body/div[2]/div[2]/div[1]/div[{i}]/div[2]/h4').text.split(" ")
@@ -53,4 +57,4 @@ class Coronavirus():
 if __name__ == "__main__":    
     bot = Coronavirus()
     bot.get_data()
-    print(bot)
+    print(bot.update())
--- run.py
+++ run.py
@@ -1,12 +1,24 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from corona_brasil import Coronavirus
+import numpy as np
+import tweepy 
+
+cons_key, cons_secret, acc_token, acc_secret = np.loadtxt('API_TOKEN.txt', dtype='str', unpack=True)
+
+auth = tweepy.OAuthHandler(cons_key, cons_secret)
+auth.set_access_token(acc_token, acc_secret)
+api = tweepy.API(auth)
 
 def run():
     """Running the coronavirus bot.
     """
     bot = Coronavirus()
     bot.get_data()
-    print(bot)
+    with open('temp.txt', 'w') as f:
+        f.write(bot.update())
+    
+    with open('temp.txt','r') as f:
+        api.update_status(f.read())
 
 scheduler = BlockingScheduler()
 scheduler.add_job(run,  'cron', hour='7-20')
